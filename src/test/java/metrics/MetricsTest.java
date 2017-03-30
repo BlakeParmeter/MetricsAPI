b6b998c903a8d77f57ec14b52ed5a4d77f66d239
Blake Lee Parmeter <blakep@knights.ucf.edu>
2017-03-30 18:08:20 -0400

    Used the fix from the stack overflow to fix the unit tests http://stackoverflow.com/questions/43128842/autowired-objects-are-null-only-when-testing

--- src/test/java/metrics/MetricsTest.java
+++ src/test/java/metrics/MetricsTest.java
@@ -15,21 +15,27 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+import org.springframework.web.context.WebApplicationContext;
 
 /**
  *
  * @author Blake 
  */
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
+@AutoConfigureMockMvc
 public class MetricsTest {
    
     private MockMvc mockMvc;
@@ -37,9 +43,12 @@ public class MetricsTest {
     @Autowired
     ObjectMapper objectMapper;
     
+    @Autowired
+    private WebApplicationContext wac;
+
     @Before
     public void setup() throws Exception {
-        mockMvc = standaloneSetup(new MetricAPI()).build();
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
     }
     
     /**
